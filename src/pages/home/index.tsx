f9c79e68ec44264a88c19d9aa1352286e401d0a9
= <lucio.furtado@g.globo>
2024-05-19 14:07:37 -0300

    feat: add home page components

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,20 @@
 import Layout from '../layout';
 import Menu from '../../components/menu';
+import BannerHome from '../../components/bannerHome';
+import Footer from '../../components/footer';
+import ValuesSection from '../../components/valuesSection';
+import ProjectsSection from '../../components/projects';
+import ContactSection from '../../components/contactSection';
 
 export default function Home() {
   return (
     <Layout title="Home Page" description="zz">
       <Menu />
-      <h1>Dilegno</h1>
+      <BannerHome />
+      <ValuesSection />
+      <ProjectsSection />
+      <ContactSection />
+      <Footer />
     </Layout>
   );
 }
